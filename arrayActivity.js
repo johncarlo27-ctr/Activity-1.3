068867ed090b09c8d66e7eb96b1e23726e17171d
unknown <johncarloaltura705@gmail.com>
2024-10-20 20:05:06 +0800

    initial commit

---
+++ arrayActivity.js
@@ -0,0 +1,17 @@
+// Create an array called favSingers and store your three favorite singers in it.
+const favSingers = ["Taylor Swift", "Arianna Grande", "Katy Perry"];
+
+// Log the first singer in the array using array index notation.
+console.log(favSingers[1]);
+
+// Create an array called favNumbers and store four of your favorite numbers.
+const favNumbers = [7, 14, 21, 28];
+
+// Create an array named mixedArr and store different data types: ["string", ["otherarray"], 123, true].
+const mixedArr = ["string", ["otherarray"], 123, true];
+
+// Access and log each item in the mixedArr using array index notation.
+console.log(mixedArr[0]); // Logs: "string"
+console.log(mixedArr[1]); // Logs: ["otherarray"]
+console.log(mixedArr[2]); // Logs: 123
+console.log(mixedArr[3]); // Logs: true
